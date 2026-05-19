a1caa6f11c289e0dc0d7ef9e1c92a55878c7c9db
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-05-19 22:59:49 +0530

    add getAllLeads

--- server/src/controllers/lead.controller.ts
+++ server/src/controllers/lead.controller.ts
@@ -2,13 +2,9 @@ import { Request, Response } from "express";
 
 import Lead from "../models/lead.model";
 
-export const createLead = async (
-  req: Request,
-  res: Response
-) => {
+export const createLead = async ( req: Request, res: Response ) => {
   try {
-    const { name, email, status, source } =
-      req.body;
+    const { name, email, status, source } = req.body;
 
     if (!name || !email || !source) {
       return res.status(400).json({
@@ -33,6 +29,82 @@ export const createLead = async (
   } catch (error) {
     res.status(500).json({
       message: "Failed to create lead",
+    });
+  }
+};
+
+export const getAllLeads = async ( req: Request, res: Response ) => {
+  try {
+    const {
+      status,
+      source,
+      search,
+      sort = "latest",
+      page = "1",
+    } = req.query;
+
+    const query: any = {};
+
+    if (status) {
+      query.status = status;
+    }
+
+    if (source) {
+      query.source = source;
+    }
+
+    if (search) {
+      query.$or = [
+        {
+          name: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+        {
+          email: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+      ];
+    }
+
+    const limit = 10;
+    const currentPage = Number(page);
+
+    const skip = (currentPage - 1) * limit;
+
+    const sortOption =
+      sort === "oldest"
+        ? { createdAt: 1 }
+        : { createdAt: -1 };
+
+    const leads = await Lead.find(query)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit);
+
+    const totalLeads = await Lead.countDocuments(
+      query
+    );
+
+    res.status(200).json({
+      success: true,
+
+      pagination: {
+        total: totalLeads,
+        page: currentPage,
+        totalPages: Math.ceil(
+          totalLeads / limit
+        ),
+      },
+
+      leads,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch leads",
     });
   }
 };
--- server/src/routes/lead.routes.ts
+++ server/src/routes/lead.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createLead } from "../controllers/lead.controller";
+import { createLead, getAllLeads } from "../controllers/lead.controller";
 import { protect } from "../middleware/auth.middleware";
 
 export const LeadRouter = Router();
 
 LeadRouter.post("/", protect, createLead);
+LeadRouter.get("/", protect, getAllLeads);

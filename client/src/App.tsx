e97bb903409b77318190bc91ca3941cafd200618
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-05-20 23:01:56 +0530

    add dashboard routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />}/>
       <Route path="/signup" element={<Signup />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
     </Routes>
   )
 }
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold">
+        Smart Leads Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
